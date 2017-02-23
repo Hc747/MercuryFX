6aaa21fdc52cd6807bca70b54ab8d12ddadf6e25
Hc747 <harrisoncole05@gmail.com>
2017-02-23 15:03:37 +1100

    Initial commit

---
+++ src/org/mercury/Mercury.java
@@ -0,0 +1,33 @@
+package org.mercury;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * @author Harrison, Alias: Hc747, Contact: harrisoncole05@gmail.com
+ * @version 1.0
+ * @since 23/2/17
+ */
+public final class Mercury extends Application {
+
+	@Override
+	public void start(Stage stage) throws IOException {
+		final Parent root = FXMLLoader.load(getClass().getResource("./login/login.fxml"));
+		stage.setTitle("Mercury");
+		stage.setResizable(false);
+
+		final Scene scene = new Scene(root);
+		stage.setScene(scene);
+
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
